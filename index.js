8b977078e4c7a334961f05387b87f27d7253d01d
Ebuka Nwabueze <ebukanwabueze@gmail.com>
2021-12-31 16:57:58 +0100

    Making use of app.use() to ensure all requests gets parsed

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import bodyParser from 'body-parser'
 
 const app = express();
 
+// ensures all requests gets parsed for middleware
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/',(req,res)=>{
     res.send(`
         <div>
@@ -16,7 +19,7 @@ app.get('/',(req,res)=>{
     `);
 });
 
-app.post('/',bodyParser.urlencoded({ extended: true }), (req,res)=>{
+app.post('/', (req,res)=>{
    console.log(req.body)
     res.send('Account created')
 });
