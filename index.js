d5620842273a2492be1d91316b5a665d5a73186a
Ebuka Nwabueze <ebukanwabueze@gmail.com>
2022-01-01 23:28:57 +0100

    signIn route created

--- index.js
+++ index.js
@@ -65,8 +65,17 @@ app.get("/signin", (req, res) => {
 `);
 });
 
-app.post('/signin', (req,res)=>{
-
+app.post('/signin', async (req,res)=>{
+    const {email, password} = req.body
+    const user = await repo.getOneBy({email})
+    if(!user){
+        return res.send('Email address not found')
+    }
+    if(user.password !== password){
+        return res.send('Password Incorrect. Try again!!!')
+    }
+    req.session.userId = user.id
+    res.send('You have been signed in')
 });
 
 app.listen(3000, () => {
