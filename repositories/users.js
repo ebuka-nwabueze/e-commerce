4ab2602c60d26ae5d504741457b2a936e677d1ba
Ebuka Nwabueze <ebukanwabueze@gmail.com>
2022-01-01 13:25:22 +0100

    Function to get all users in database

--- repositories/users.js
+++ repositories/users.js
@@ -1,17 +1,34 @@
-import fs from 'fs';
+import fs from "fs";
 
-class UsersRepository{
-    constructor(filename){
-        if (!filename){
-            throw new Error('Creating a repository requires a filename')
-        }
-        this.filename = filename
-        try {
-            fs.accessSync(this.filename)
-        } catch (error) {
-            fs.writeFileSync(this.filename, '[]')
-        }
+class UsersRepository {
+  constructor(filename) {
+    // check if a filename was passed call this class
+    if (!filename) {
+      throw new Error("Creating a repository requires a filename");
     }
+    this.filename = filename;
+    // check if the file exist within the current directory
+    //the class was called and create one if it does not exist.
+    // Synchronous is used to check immediately before it continues that it exist. it is the best case for
+    try {
+      fs.accessSync(this.filename);
+    } catch (error) {
+      fs.writeFileSync(this.filename, '[]');
+    }
+  }
+  // promise function to have additionally, good to have outside the constructor.
+  // it doesn't need to run immediately on class instatiation.
+  async getAll() {
+    return JSON.parse(await fs.promises.readFile(this.filename, { encoding: 'utf8' }))
+  }; //** end of get all
 }
 
-new UsersRepository('users.json')
+const test = async () => {
+    const repo = new UsersRepository("users.json");
+
+    const users = await repo.getAll();
+    console.log(users)
+};
+
+test();
+
