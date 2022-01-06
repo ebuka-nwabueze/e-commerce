57de672f1bb675ea211c69bc7b30cdce4eee75df
Ebuka Nwabueze <ebukanwabueze@gmail.com>
2022-01-06 15:06:50 +0100

    multer included

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,6 +1,7 @@
 // external libraries 
 import express from "express";
 import  { validationResult} from 'express-validator'
+import multer from "multer";
 
 // local import
 import { productRepo } from "../../repositories/products.js";
@@ -8,6 +9,7 @@ import productNewTemplate from "../../views/admin/products/new.js";
 import { requireTitle, requirePrice } from "../../routes/admin/validators.js";
 
 const router = express.Router();
+const upload = multer({storage: multer.memoryStorage()})
 
 router.get("/admin/products", (req, res) => {});
 
@@ -15,7 +17,7 @@ router.get("/admin/products/new", (req, res) => {
   res.send(productNewTemplate({}));
 });
 
-router.post("/admin/products/new", [requireTitle, requirePrice], (req, res) => {
+router.post("/admin/products/new", [requireTitle, requirePrice], upload.single('image') ,(req, res) => {
     const errors = validationResult(req)
     console.log(errors)
     // res.send(productNewTemplate({}));
